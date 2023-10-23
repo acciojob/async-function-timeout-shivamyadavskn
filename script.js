643920abf3849d7bd9a8c198cf506fc3d5d7be73
Shivam Yadav <117904778+shivamyadavskn@users.noreply.github.com>
2023-10-23 14:00:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-//your JS code here. If required.
+async function displayTextWithDelay() {
+  const textInput = document.getElementById("text").value;
+  const delayInput = parseInt(document.getElementById("delay").value);
+
+  const outputDiv = document.getElementById("output");
+  outputDiv.textContent = "Waiting...";
+
+  // Use a Promise to wait for the specified delay
+  function delay(timeout) {
+    return new Promise(resolve => setTimeout(resolve, timeout));
+  }
+
+  // Wait for the specified delay
+  await delay(delayInput);
+
+  // Display the text after the delay
+  outputDiv.textContent = textInput;
+}
+
+const displayButton = document.getElementById("btn");
+displayButton.addEventListener("click", displayTextWithDelay);
